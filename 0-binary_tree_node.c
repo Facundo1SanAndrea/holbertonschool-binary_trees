f3c265c8614959a77e9e631210bba76051255ef9
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-02 10:37:40 -0700

    Adding functions

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_t - function that creates a binary tree node
+ *binary_tree_node - function that creates a binary tree node
  *@parent: pointer to the parent node
  *@value: value put to the new node
  *Return: pointer to new node.
